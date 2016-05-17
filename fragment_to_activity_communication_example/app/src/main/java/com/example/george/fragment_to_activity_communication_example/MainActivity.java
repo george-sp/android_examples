32cf8f3d0ea8fed64654c1a73bcfa76d38a5b7d0
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-18 00:44:47 +0300

    Modify MainActivity.onCreate().

--- fragment_to_activity_communication_example/app/src/main/java/com/example/george/fragment_to_activity_communication_example/MainActivity.java
+++ fragment_to_activity_communication_example/app/src/main/java/com/example/george/fragment_to_activity_communication_example/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.george.fragment_to_activity_communication_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+    private PagerAdapter mPagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ArrayList<String> tabs = new ArrayList<>();
+        tabs.add("Tab A");
+        tabs.add("Tab B");
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabs);
+        viewPager.setAdapter(mPagerAdapter);
+        mTabLayout.setupWithViewPager(viewPager);
     }
 }
