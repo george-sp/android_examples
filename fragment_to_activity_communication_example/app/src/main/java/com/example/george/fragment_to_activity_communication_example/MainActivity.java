5cf5da85f5127d61e1482e2a78a8eb042d890bd6
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-18 00:55:01 +0300

    Handle menu actions in MainActivity.

--- fragment_to_activity_communication_example/app/src/main/java/com/example/george/fragment_to_activity_communication_example/MainActivity.java
+++ fragment_to_activity_communication_example/app/src/main/java/com/example/george/fragment_to_activity_communication_example/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.george.fragment_to_activity_communication_example;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,4 +38,37 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(mPagerAdapter);
         mTabLayout.setupWithViewPager(viewPager);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            int position = mTabLayout.getSelectedTabPosition();
+            Fragment fragment = mPagerAdapter.getFragment(mTabLayout.getSelectedTabPosition());
+            if (fragment != null) {
+                switch (position) {
+                    case 0:
+                        ((TabFragmentA) fragment).onRefresh();
+                        break;
+                    case 1:
+                        ((TabFragmentB) fragment).onRefresh();
+                        break;
+                }
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
 }
