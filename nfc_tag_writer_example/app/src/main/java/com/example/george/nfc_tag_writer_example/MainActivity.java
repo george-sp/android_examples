ce690c9fa1af2679e6b4adea8cafad2d30c382d9
george_spiridakis <spiridakis.grg@gmail.com>
2016-05-19 23:34:00 +0300

    Design activity_main layout.

--- nfc_tag_writer_example/app/src/main/java/com/example/george/nfc_tag_writer_example/MainActivity.java
+++ nfc_tag_writer_example/app/src/main/java/com/example/george/nfc_tag_writer_example/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.george.nfc_tag_writer_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
+/**
+ * @author George Spiridakis <george@codeburrow.com>
+ * @since May/19/2016.
+ * ===================================================
+ * ---------->    http://codeburrow.com    <----------
+ * ===================================================
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    EditText mEditMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditMessage = (EditText) findViewById(R.id.edit_message);
+    }
+
+    public void writeToTag(View view) {
     }
 }
