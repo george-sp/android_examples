105d2e6fbdbf5a8b976b4adc35149acaaf5fa46d
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-05 22:13:10 +0300

    Start Example: AndroidPay.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android_examples
 Some basic and useful android examples
+
+---------------------------------------
+- [AndroidPay](https://github.com/george-sp/android_examples/tree/master/android_pay_example)
---
+++ android_pay_example/app/src/main/java/com/codeburrow/android_pay_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codeburrow.android_pay_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
