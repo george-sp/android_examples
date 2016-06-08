0deb2d22117ecb712b8e98c687379385fc78853f
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-08 22:51:46 +0300

    Start recording audio.

--- audio_capture_example/app/src/main/java/com/codeburrow/audio_capture_example/MainActivity.java
+++ audio_capture_example/app/src/main/java/com/codeburrow/audio_capture_example/MainActivity.java
@@ -1,13 +1,82 @@
 package com.codeburrow.audio_capture_example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private Button recordButton;
+    private Button stopButton;
+    private Button playButton;
+
+    private MediaRecorder mMediaRecorder;
+
+    private String outputFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recordButton = (Button) findViewById(R.id.record_button);
+        stopButton = (Button) findViewById(R.id.stop_button);
+        playButton = (Button) findViewById(R.id.play_button);
+
+        // Disable play and stop buttons.
+        playButton.setEnabled(false);
+        stopButton.setEnabled(false);
+
+        // The pathname to use.
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+
+        // Create a new instance of MediaRecorder.
+        mMediaRecorder = new MediaRecorder();
+        // Sets the audio source to be used for recording.
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        // Sets the format of the output file produced during recording.
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        // Sets the audio encoder to be used for recording.
+        mMediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        // Sets the path of the output file to be produced.
+        mMediaRecorder.setOutputFile(outputFile);
+    }
+
+    public void recordAudio(View view) {
+        try {
+            // Prepare the recorder to begin capturing and encoding data.
+            // This method must be called after setting up the desired audio and video sources, encoders, file format, etc., but before start().
+            mMediaRecorder.prepare();
+            // Begins capturing and encoding data to the file specified with setOutputFile().
+            // Call this after prepare().
+            mMediaRecorder.start();
+        } catch (IllegalStateException e) {
+            // Throws this exception if it is called after start() or before setOutputFormat().
+            Log.e(LOG_TAG, e.getMessage());
+        } catch (IOException e) {
+            // Throws this exception if prepare fails otherwise.
+            Log.e(LOG_TAG, e.getMessage());
+        }
+
+        recordButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        Toast.makeText(MainActivity.this, "Recording started", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void stopAudio(View view) {
+    }
+
+    public void playAudio(View view) {
     }
 }
