d81445a7c2bd3db2ee8e3852bfc6b714af012359
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-18 14:06:46 +0300

    Use a ViewHolder to improve performance.

--- file_chooser_example/app/src/main/java/com/codeburrow/file_chooser_example/FileArrayAdapter.java
+++ file_chooser_example/app/src/main/java/com/codeburrow/file_chooser_example/FileArrayAdapter.java
@@ -20,6 +20,25 @@ public class FileArrayAdapter extends ArrayAdapter<Properties> {
 
     private static final String LOG_TAG = FileArrayAdapter.class.getSimpleName();
 
+    /**
+     * What the ViewHolder class does is cache the call to findViewById().
+     * <p/>
+     * Once your ListView has reached the max amount of rows it can display on a screen,
+     * Android is smart enough to begin recycling those row Views.
+     * <p/>
+     * We check if a View is recycled with if (convertView == null).
+     * If it is not null then we have a recycled View and can just change its values,
+     * otherwise we need to create a new row View.
+     * <p/>
+     * The magic behind this is the setTag() method
+     * which lets us attach an arbitrary object onto a View object,
+     * which is how we save the already inflated View for future reuse.
+     */
+    private static class ViewHolder {
+
+        TextView name;
+        TextView data;
+    }
 
     public FileArrayAdapter(Context context, ArrayList<Properties> propertiesArrayList) {
         super(context, 0, propertiesArrayList);
@@ -27,19 +46,25 @@ public class FileArrayAdapter extends ArrayAdapter<Properties> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
+        // Get the data item for this position.
         Properties properties = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
+        // Check if an existing view is being reused, otherwise inflate the view.
+        ViewHolder viewHolder; // View lookup cache stored it tag.
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_file, parent, false);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_item_file, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.name_property_textview);
+            viewHolder.data = (TextView) convertView.findViewById(R.id.data_property_textview);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        // Lookup view for data population
-        TextView nameTextView = (TextView) convertView.findViewById(R.id.name_property_textview);
-        TextView dateTextView = (TextView) convertView.findViewById(R.id.data_property_textview);
-        // Populate the data into the template view using the data object
-        nameTextView.setText(properties.getName());
-        dateTextView.setText(properties.getData());
-        // Return the completed view to render on screen
+
+        // Populate the data into the template view using the data object.
+        viewHolder.name.setText(properties.getName());
+        viewHolder.data.setText(properties.getData());
+        // Return the completed view to render on screen.
         return convertView;
     }
 }
