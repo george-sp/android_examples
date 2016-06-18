56584e95ad800e7da2604c1a5374565a5d99edec
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-18 13:20:51 +0300

    Show file/folders using a Simple Array Adapter.

--- file_chooser_example/app/src/main/java/com/codeburrow/file_chooser_example/FileChooserActivity.java
+++ file_chooser_example/app/src/main/java/com/codeburrow/file_chooser_example/FileChooserActivity.java
@@ -2,8 +2,12 @@ package com.codeburrow.file_chooser_example;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FileChooserActivity extends ListActivity {
 
@@ -27,6 +31,45 @@ public class FileChooserActivity extends ListActivity {
      * @param file The current directory
      */
     private void fillFromDir(File file) {
+        // Get an array of all files and folders in the current dir
+        File[] dirs = file.listFiles();
+        this.setTitle("Current Dir: " + file.getName());
 
+        // Create array list for folders
+        ArrayList<Properties> folders = new ArrayList<>();
+        // Create array list for folders
+        ArrayList<Properties> files = new ArrayList<>();
+
+        // Add files and folders to the appropriate array list.
+        try {
+            for (File ff : dirs) {
+                if (ff.isDirectory())
+                    folders.add(new Properties(ff.getName(), "Folder", ff.getAbsolutePath()));
+                else {
+                    files.add(new Properties(ff.getName(), "File Size: " + ff.length(), ff.getAbsolutePath()));
+                }
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+
+        // Sort the array lists alphabetically.
+        Collections.sort(folders);
+        Collections.sort(files);
+
+        // Add files array list to the folders(array list).
+        folders.addAll(files);
+
+        // An array list for debugging.
+        ArrayList<String> directoriesNames = new ArrayList<>();
+        for (Properties properties : folders) {
+            directoriesNames.add(properties.getName());
+        }
+
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, directoriesNames));
+
+        if (!file.getName().equalsIgnoreCase("sdcard"))
+            folders.add(0, new Properties("..", "Parent Directory", file.getParent()));
     }
+
 }
