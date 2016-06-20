ad7f7507e6e58bee039a7936ebc5aa124b329bfd
George Spiridakis <spiridakis.grg@gmail.com>
2016-06-20 23:48:52 +0300

    Declare all CRUD operations.

--- sqlite_database_example/app/src/main/java/com/codeburrow/sqlite_database_example/ContactsDBHelper.java
+++ sqlite_database_example/app/src/main/java/com/codeburrow/sqlite_database_example/ContactsDBHelper.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.codeburrow.sqlite_database_example.DatabaseContract.ContactsEntry;
 
+import java.util.ArrayList;
+
 /**
  * @author George Spiridakis <george@codeburrow.com>
  * @since Jun/20/2016.
@@ -60,5 +62,68 @@ public class ContactsDBHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ContactsEntry.TABLE_NAME);
         // Create table again.
         onCreate(sqLiteDatabase);
+    }
+
+    /*
+     * All CRUD (Create, Read, Update, Delete) Operations.
+     */
+
+    /**
+     * Insert NEW Record-Contact into the SQLite Database.
+     *
+     * @param contact The ContactDAO to be inserted.
+     * @return The row ID of the newly inserted row, or -1 if an error occurred.
+     */
+    public long createContact(ContactDAO contact) {
+        return -1;
+    }
+
+    /**
+     * Read SINGLE Record-Contact from the SQLiteDatabase.
+     *
+     * @param id The ID of the requested contact.
+     * @return A Contact Data Access Object.
+     */
+    public ContactDAO readContact(int id) {
+        return new ContactDAO();
+    }
+
+    /**
+     * Update SINGLE Record-Contact from the SQLiteDatabase.
+     *
+     * @param contact The ContactDAO to be updated.
+     * @return The number of rows affected.
+     */
+    public int updateContact(ContactDAO contact) {
+        return 0;
+    }
+
+    /**
+     * Delete SINGLE Record-Contact from the SQLiteDatabase.
+     * To remove all rows and get a count pass "1" as the whereClause.
+     *
+     * @param contact
+     * @return The number of rows affected if a whereClause is passed in, 0 otherwise.
+     */
+    public int deleteContact(ContactDAO contact) {
+        return 0;
+    }
+
+    /**
+     * Read ALL Records-Contacts from the SQLiteDatabase.
+     *
+     * @return A List with the ContactDAO objects.
+     */
+    public ArrayList<ContactDAO> readAllContacts() {
+        return new ArrayList<>();
+    }
+
+    /**
+     * Count the Records-Contacts of the SQLiteDatabase.
+     *
+     * @return The number of rows - stored contacts.
+     */
+    public int countContacts() {
+        return 0;
     }
 }
