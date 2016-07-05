5306aab235dbe9046944017e88852ef45b979d83
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-05 13:58:12 +0300

    Start Camera Preview.

--- camera_basics_example/app/src/main/java/com/codeburrow/camera_basics_example/CameraView.java
+++ camera_basics_example/app/src/main/java/com/codeburrow/camera_basics_example/CameraView.java
@@ -2,8 +2,11 @@ package com.codeburrow.camera_basics_example;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.IOException;
 
 /**
  * @author George Spiridakis <george@codeburrow.com>
@@ -27,7 +30,7 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
      * - monitor changes to the surface
      * This interface is typically available through the SurfaceView class.
      */
-    private SurfaceHolder mHolder;
+    private SurfaceHolder mSurfaceHolder;
     /*
      * The Camera class is used to:
      * - set image capture settings
@@ -44,25 +47,41 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
         mCamera = camera;
         // Get the SurfaceHolder providing access
         // and control over this SurfaceView's underlying surface.
-        mHolder = getHolder();
+        mSurfaceHolder = getHolder();
         // Add a callback interface for this holder.
-        mHolder.addCallback(this);
+        mSurfaceHolder.addCallback(this);
         // Deprecated setting, but required on Android versions prior to 3.0.
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code they desire.
+     *
+     * Note:
+     *      Only one thread can ever draw into a Surface,
+     *      so you should not draw into the Surface here
+     *      if your normal rendering will be in another thread.
+     *
+     * @param surfaceHolder The SurfaceHolder whose surface is being created.
+     */
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+        try {
+            // Set the Surface to be used for live preview.
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+            // Start capturing and drawing preview frames to the screen.
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "CameraView Error on surfaceCreated:\n" + e.getMessage());
+        }
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
     }
 }
--- camera_basics_example/app/src/main/java/com/codeburrow/camera_basics_example/MainActivity.java
+++ camera_basics_example/app/src/main/java/com/codeburrow/camera_basics_example/MainActivity.java
@@ -1,13 +1,48 @@
 package com.codeburrow.camera_basics_example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private Camera mCamera;
+    private CameraView mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         * Retrieve the current Window for the Activity or null if it is not visual.
+         * Set Window Flag: as long as this window is visible to the user,
+         *                  keep the device's screen turned on and bright.
+         */
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        try{
+            // You can use open(int) to use different cameras.
+            mCamera = Camera.open();
+        } catch (Exception e){
+            Log.e(LOG_TAG, "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            // Create a SurfaceView to show camera data.
+            mCameraView = new CameraView(this, mCamera);
+            // Find the FrameLayout in the activity's layout.
+            FrameLayout frameLayout = (FrameLayout)findViewById(R.id.camera_view_frame_layout);
+            // Add the SurfaceView to the FrameLayout.
+            frameLayout.addView(mCameraView);
+        } else {
+            Log.e(LOG_TAG, "Failed to create the SurfaceView");
+        }
+
     }
 }
