e341dd9bda9d6107c6e4112ecbbad5a6026d9178
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-15 01:11:04 +0300

    Explicitly set the component to handle the Intent.

--- intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityRed.java
+++ intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityRed.java
@@ -1,7 +1,10 @@
 package com.codeburrow.intents_basic_example;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * @author George Spiridakis <george@codeburrow.com>
@@ -10,7 +13,7 @@ import android.support.v7.app.AppCompatActivity;
  * ---------->    http://codeburrow.com    <----------
  * ===================================================
  */
-public class ActivityRed extends AppCompatActivity{
+public class ActivityRed extends AppCompatActivity {
 
     private static final String LOG_TAG = ActivityRed.class.getSimpleName();
 
@@ -18,5 +21,37 @@ public class ActivityRed extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_red);
+    }
+
+    public void launchActivityBlue(View view) {
+        /*
+         * Create an empty Intent.
+         * Explicitly set the component to handle the intent (Usually Optional).
+         * - setComponent(ComponentName component)
+         * - setClass(Context packageContext, Class<?> class)
+         * - setClassName(String packageName, String className)
+         * - setClassName(Context packageContext, String className)
+         */
+        // Create an empty Intent, using the empty constructor.
+        Intent launchActivityBlueIntent = new Intent();
+        /*
+         * setComponent
+         * Use setComponent() - explicitly set the component to handle the intent.
+         */
+        // Create a new component identifier from a Context and class name.
+        ComponentName activityBlueComponentName = new ComponentName(this, ActivityBlue.class);
+        launchActivityBlueIntent.setComponent(activityBlueComponentName);
+        /*
+         * setClass
+         * Use setClass() - convenience for calling setComponent() with the name returned by a Class object.
+         */
+//        launchActivityBlueIntent.setClass(this, ActivityBlue.class);
+        /*
+         * setClassName
+         * Use setClassName() - convenience for calling setComponent() with an explicit application package name and class name.
+         */
+//        launchActivityBlueIntent.setClassName("com.codeburrow.intents_basic_example", "com.codeburrow.intents_basic_example.ActivityBlue");
+        // Launch ActivityRed activity.
+        startActivity(launchActivityBlueIntent);
     }
 }
