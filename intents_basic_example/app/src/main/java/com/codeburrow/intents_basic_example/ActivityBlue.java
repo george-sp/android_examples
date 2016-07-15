97ae0dd8eb97d45eb8de818d7cad791749d21daa
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-15 19:23:05 +0300

    Add extended (Extra) data to an intent.

--- intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityBlue.java
+++ intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityBlue.java
@@ -4,21 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class ActivityBlue extends AppCompatActivity {
 
     private static final String LOG_TAG = ActivityBlue.class.getSimpleName();
+    private EditText mEditText;
+    public static String USER_TEXT_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue);
+
+        mEditText = (EditText) findViewById(R.id.edit_text);
     }
 
     public void launchActivityRed(View view) {
         /*
          * Explicit Intent:
-         *                  An application can define the target component
+         *
+             *              An application can define the target component
          *                  directly in the intent.
          *
          * An explicit intent is most commonly used when launching an activity within the same application.
@@ -33,5 +39,15 @@ public class ActivityBlue extends AppCompatActivity {
         // Request that a given application service be stopped.
         // If the service is not running, nothing happens.
         stopService(new Intent(ActivityBlue.this, CustomService.class));
+    }
+
+    public void launchActivityRedDataTransfer(View view) {
+        Intent launchActivityRedIntent = new Intent(ActivityBlue.this, ActivityRed.class);
+        /*
+         * Add extended data to the intent.
+         *
+         */
+        launchActivityRedIntent.putExtra(USER_TEXT_KEY, mEditText.getText().toString());
+        startActivity(launchActivityRedIntent);
     }
 }
--- intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityRed.java
+++ intents_basic_example/app/src/main/java/com/codeburrow/intents_basic_example/ActivityRed.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author George Spiridakis <george@codeburrow.com>
@@ -21,6 +22,14 @@ public class ActivityRed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_red);
+
+        // Get the extra data in a Bundle object.
+        Bundle extras = getIntent().getExtras();
+        // Check if there are extra data.
+        if (extras != null) {
+            String transferredText = extras.getString(ActivityBlue.USER_TEXT_KEY);
+            Toast.makeText(this, transferredText, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void launchActivityBlue(View view) {
