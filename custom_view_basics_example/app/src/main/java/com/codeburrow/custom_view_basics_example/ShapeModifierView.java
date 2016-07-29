9d70eb3d7d2a6421f5bcf6ec4e6b1c0eb8b2db38
George Spiridakis <spiridakis.grg@gmail.com>
2016-07-29 16:08:23 +0300

    Retrieve the Custom Attributes from the XML.

--- custom_view_basics_example/app/src/main/java/com/codeburrow/custom_view_basics_example/ShapeModifierView.java
+++ custom_view_basics_example/app/src/main/java/com/codeburrow/custom_view_basics_example/ShapeModifierView.java
@@ -1,7 +1,10 @@
 package com.codeburrow.custom_view_basics_example;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -15,6 +18,12 @@ public class ShapeModifierView extends View {
 
     private static final String LOG_TAG = ShapeModifierView.class.getSimpleName();
 
+    private Context mContext;
+
+    private int mShapeColor;
+    private String mShapeName;
+    private boolean mDisplayShapeName;
+
     /**
      * Constructor that is called when inflating a view from XML file,
      * supplying attributes that were specified in the XML file.
@@ -24,5 +33,40 @@ public class ShapeModifierView extends View {
      */
     public ShapeModifierView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mContext = context;
+        retrieveCustomAttributes(attrs);
+    }
+
+    /**
+     * Retrieve the Custom Attributes from the XML file.
+     *
+     * @param attrs The desired attributes to be retrieved.
+     */
+    private void retrieveCustomAttributes(AttributeSet attrs) {
+        /*
+         * TypedArray: Container for an array of values that were retrieved with obtainStyledAttributes(...).
+         * Be sure to call recycle() when done with them.
+         *
+         * obtainStyledAttributes: Returns a TypedArray holding an array of the attribute values.
+         */
+        // Obtain a TypedArray of the attribute values.
+        TypedArray attrsTypedArray = mContext.getTheme().obtainStyledAttributes(attrs, R.styleable.ShapeModifierView, 0, 0);
+        try {
+            // Retrieve the color value for the attribute at index: ShapeModifierView_shapeColor.
+            mShapeColor = attrsTypedArray.getColor(R.styleable.ShapeModifierView_shapeColor, Color.BLACK);
+            // Retrieve the string value for the attribute at index: ShapeModifierView_shapeName.
+            mShapeName = attrsTypedArray.getString(R.styleable.ShapeModifierView_shapeName);
+            // Retrieve the boolean value for the attribute at index: ShapeModifierView_displayShapeName.
+            mDisplayShapeName = attrsTypedArray.getBoolean(R.styleable.ShapeModifierView_displayShapeName, false);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        } finally {
+            /*
+             * Recycle the TypedArray, to be re-used by a later caller.
+             * After calling this function you must not ever touch the typed array again.
+             */
+            attrsTypedArray.recycle();
+        }
     }
 }
