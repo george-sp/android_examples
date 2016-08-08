f5b7c9bc9b7477282656eeb6678abaf47fd8023a
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-08 20:20:44 +0300

    Perform a Simple Transition.

--- transitions_example/app/src/main/java/com/codeburrow/transitions_example/TransitionFragment.java
+++ transitions_example/app/src/main/java/com/codeburrow/transitions_example/TransitionFragment.java
@@ -3,6 +3,8 @@ package com.codeburrow.transitions_example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.transition.Scene;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +24,19 @@ public class TransitionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.scene_start, container, false);
+
+        // Get the Scene described by the resource file associated with the given layoutId parameter.
+        final Scene endScene = Scene.getSceneForLayout(container, R.layout.scene_end, getActivity());
+
+        Button transitionButton = (Button) rootView.findViewById(R.id.transition_button);
+        transitionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Change to the given scene using the default transition for TransitionManager.
+                TransitionManager.go(endScene);
+            }
+        });
+
         return rootView;
     }
 }
