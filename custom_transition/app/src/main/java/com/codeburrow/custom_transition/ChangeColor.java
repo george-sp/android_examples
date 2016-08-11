b1cb93d63f997857c2909920a9ec606eca1df479
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-11 14:02:01 +0300

    Extend the Transition class to create a Custom Transition.

---
+++ custom_transition/app/src/main/java/com/codeburrow/custom_transition/ChangeColor.java
@@ -0,0 +1,23 @@
+package com.codeburrow.custom_transition;
+
+import android.transition.Transition;
+import android.transition.TransitionValues;
+
+/**
+ * A Custom Transition enables you to create animations that are not available
+ * from any of the built-in transition classes.
+ */
+public class ChangeColor extends Transition {
+
+    private static final String LOG_TAG = ChangeColor.class.getSimpleName();
+
+    @Override
+    public void captureStartValues(TransitionValues transitionValues) {
+
+    }
+
+    @Override
+    public void captureEndValues(TransitionValues transitionValues) {
+
+    }
+}
