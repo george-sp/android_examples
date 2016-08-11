6c46edbffb29c61dc6efd0680db59c8473d84fa3
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-11 14:08:22 +0300

    Capture starting and ending view values.

--- custom_transition/app/src/main/java/com/codeburrow/custom_transition/ChangeColor.java
+++ custom_transition/app/src/main/java/com/codeburrow/custom_transition/ChangeColor.java
@@ -11,13 +11,39 @@ public class ChangeColor extends Transition {
 
     private static final String LOG_TAG = ChangeColor.class.getSimpleName();
 
-    @Override
-    public void captureStartValues(TransitionValues transitionValues) {
+    /**
+     * Key to store a color value in TransitionValues object
+     */
+    private static final String PROPNAME_BACKGROUND = "customtransition:change_color:background";
 
+    /**
+     * Convenience method: Add the background Drawable property value
+     * to the TransitionsValues.value Map for a target.
+     */
+    private void captureValues(TransitionValues values) {
+        // Capture the property values of views for later use.
+        values.values.put(PROPNAME_BACKGROUND, values.view.getBackground());
     }
 
+    /**
+     * Captures the values in the start scene for the properties
+     * that this transition monitors.
+     *
+     * @param transitionValues The holder for any values that the Transition wishes to store.
+     */
+    @Override
+    public void captureStartValues(TransitionValues transitionValues) {
+        captureValues(transitionValues);
+    }
+
+    /**
+     * Captures the values in the end scene for the properties
+     * that this transition monitors.
+     *
+     * @param transitionValues The holder for any values that the Transition wishes to store.
+     */
     @Override
     public void captureEndValues(TransitionValues transitionValues) {
-
+        captureValues(transitionValues);
     }
 }
