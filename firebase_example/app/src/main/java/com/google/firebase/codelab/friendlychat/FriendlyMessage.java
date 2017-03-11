240ee1ec14541cabc7b029c14476fbf27d42f1c6
George Spyridakis <spiridakis.grg@gmail.com>
2017-03-11 22:43:33 +0200

    Send messages to Firebase.
    Implement text message sending.
    Implement image message sending.
    	- Select image
    	- Handle image selection
    	- Write temporary image message to the Real Time Database
    	- Begin to upload selected image
    	- Update image message URL to that of the uploaded image, once upload is complete

--- firebase_example/app/src/main/java/com/google/firebase/codelab/friendlychat/FriendlyMessage.java
+++ firebase_example/app/src/main/java/com/google/firebase/codelab/friendlychat/FriendlyMessage.java
@@ -1,12 +1,12 @@
 /**
  * Copyright Google Inc. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,12 @@ public class FriendlyMessage {
     private String imageUrl;
 
     public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String text, String name, String photoUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
     }
 
     public FriendlyMessage(String text, String name, String photoUrl, String imageUrl) {
