9a42cb32121877475c78f0fc98d2dbd9985ce83a
George Spyridakis <spiridakis.grg@gmail.com>
2017-09-28 22:25:46 +0300

    Add the RecyclerView

--- carousel_layout_manager_example/app/src/main/java/com/spyridakis/carousel_layout_manager_example/MainActivity.java
+++ carousel_layout_manager_example/app/src/main/java/com/spyridakis/carousel_layout_manager_example/MainActivity.java
@@ -2,6 +2,7 @@ package com.spyridakis.carousel_layout_manager_example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
         toolbar.setLogoDescription(getResources().getString(R.string.logo_description));
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
     }
 
     @Override
