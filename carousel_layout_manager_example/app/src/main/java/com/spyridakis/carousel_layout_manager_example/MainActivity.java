5c7d27b2deaa8ad2c9237d4523656e362a073cbe
George Spyridakis <spiridakis.grg@gmail.com>
2017-09-28 22:14:37 +0300

    Set up a Toolbar at the top

--- carousel_layout_manager_example/app/src/main/java/com/spyridakis/carousel_layout_manager_example/MainActivity.java
+++ carousel_layout_manager_example/app/src/main/java/com/spyridakis/carousel_layout_manager_example/MainActivity.java
@@ -1,7 +1,9 @@
 package com.spyridakis.carousel_layout_manager_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(null);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setLogoDescription(getResources().getString(R.string.logo_description));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
