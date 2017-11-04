de35ac7dd8f2a391b5e5544a082c5b686e4b008b
George Spyridakis <spiridakis.grg@gmail.com>
2017-11-04 23:21:13 +0200

    Use type converters to store Dates as timestamps in Loan

--- room_persistence_library/app/src/main/java/com/spyridakis/room_persistence_library/entity/Loan.java
+++ room_persistence_library/app/src/main/java/com/spyridakis/room_persistence_library/entity/Loan.java
@@ -4,6 +4,9 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.TypeConverters;
+
+import com.spyridakis.room_persistence_library.converter.DateConverter;
 
 import java.util.Date;
 
@@ -13,6 +16,7 @@ import java.util.Date;
             @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "user_id")
         }
 )
+@TypeConverters(DateConverter.class)
 //@formatter:on
 public class Loan {
 
